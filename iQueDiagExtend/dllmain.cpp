cef636ee3bd18bffc276a22ae490242e4a917533
Normmatt <normmatt234@gmail.com>
2018-03-09 18:37:08 +1300

    Add a few new commands for Dumping Files, Writing Files (Only in dev mode), Listing files on the nand.

--- iQueDiagExtend/dllmain.cpp
+++ iQueDiagExtend/dllmain.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include "include\MinHook.h"
 
+//#define DEVELOPER_MODE
+
 typedef int(*CommandHandler_func)(char* input);
 CommandHandler_func origCommandHandler;
 
@@ -46,6 +48,130 @@ int CmdDumpNandRaw()
 	return 0;
 }
 
+int CmdDumpFile(char *str)
+{
+	int* diag_handle = (int*)0x40E198;
+	int* handlesBase = (int*)0x4326C0;
+	const auto __BBC_CheckHandle = (int(*)(int handle))(0x403A20);
+	const auto __BBC_ObjectSize = (int(*)(int vtable, char *path))(0x407460);
+	const auto __BBC_GetObject = (int(*)(int vtable, char *path, unsigned char*buff, unsigned int size))(0x4074D0);
+
+	int res = __BBC_CheckHandle(*diag_handle);
+	if (res)
+	{
+		printf("__BBC_CheckHandle failed, did you init with BBCInit (B)?\n");
+		return 0;
+	}
+
+	char filename[256];
+	if (sscanf(str, "%s", &filename) >= 1)
+	{
+		int fileSize = __BBC_ObjectSize(*diag_handle, filename);
+		printf("fileSize = %08X (%d) bytes\n", fileSize, fileSize);
+		if (fileSize > 0)
+		{
+			unsigned char *buffer = (unsigned char*)malloc(fileSize);
+
+			printf("__BBC_GetObject started\n", fileSize, fileSize);
+			__BBC_GetObject(*diag_handle, filename, buffer, fileSize);
+			printf("__BBC_GetObject finished\n", fileSize, fileSize);
+
+			FILE *f = fopen(filename, "wb");
+			fwrite(buffer, 1, fileSize, f);
+			fclose(f);
+
+		}
+	}
+
+	return 0;
+}
+
+int CmdWriteFile(char *str)
+{
+	int* diag_handle = (int*)0x40E198;
+	int* handlesBase = (int*)0x4326C0;
+	const auto __BBC_CheckHandle = (int(*)(int handle))(0x403A20);
+	const auto __BBC_ObjectSize = (int(*)(int vtable, char *path))(0x407460);
+	const auto __BBC_GetObject = (int(*)(int vtable, char *path, unsigned char*buff, unsigned int size))(0x4074D0);
+	const auto __BBC_SetObject = (int(*)(int vtable, char *path, char *temp_path, unsigned char*buff, unsigned int size))(0x407550);
+
+	int res = __BBC_CheckHandle(*diag_handle);
+	if (res)
+	{
+		printf("__BBC_CheckHandle failed, did you init with BBCInit (B)?\n");
+		return 0;
+	}
+
+	char filename[256];
+	if (sscanf(str, "%s", &filename) >= 1)
+	{
+		FILE *f = fopen(filename, "rb");
+
+		if (f)
+		{
+			fseek(f, 0, SEEK_END);
+			int fileSize = ftell(f);
+			fseek(f, 0, SEEK_SET);
+
+			printf("__BBC_SetObject fileSize = %d bytes\n", fileSize);
+
+			unsigned char *buffer = (unsigned char*)malloc(fileSize);
+			fread(buffer, 1, fileSize, f);
+			fclose(f);
+
+			printf("__BBC_SetObject started\n");
+			__BBC_SetObject(*diag_handle, filename, "temp.tmp", buffer, fileSize);
+			printf("__BBC_SetObject finished\n");
+
+			free(buffer);
+		}
+		else
+		{
+			printf("%s was not found.\n", filename);
+		}
+	}
+
+	return 0;
+}
+
+int CmdGetDirListing()
+{
+	int* diag_handle = (int*)0x40E198;
+	int* handlesBase = (int*)0x4326C0;
+	const auto __BBC_CheckHandle = (int(*)(int handle))(0x403A20);
+	const auto __BBC_FReadDir = (int(*)(int vtable, unsigned char*buff, unsigned int size))(0x4096B0);
+
+	int res = __BBC_CheckHandle(*diag_handle);
+	if (res)
+	{
+		printf("__BBC_CheckHandle failed, did you init with BBCInit (B)?\n");
+		return 0;
+	}
+
+	unsigned char *buffer = (unsigned char*)calloc(0x2008, sizeof(unsigned char));
+	if (buffer)
+	{
+		printf("__BBC_FReadDir started\n");
+		__BBC_FReadDir(handlesBase[*diag_handle], buffer, 410);
+		printf("__BBC_FReadDir finished\n");
+
+		int idx = 0;
+		while (buffer[idx] != 0)
+		{
+			printf("\"%s\" \t\t size = %d bytes\n", &buffer[idx], *(unsigned int*)&buffer[idx + 0x10]);
+			idx += 0x14;
+		}
+
+		//FILE *f = fopen("readdir.bin", "wb");
+		//fwrite(buffer, 1, 0x2008, f);
+		//fclose(f);
+	}
+	free(buffer);
+
+	return 0;
+}
+
+
 int __cdecl CommandHandlerHook(char* input)
 {
 	char inputChar = *input;
@@ -56,9 +182,25 @@ int __cdecl CommandHandlerHook(char* input)
 	case 'x':
 		printf("\tiQueDiagExtend Commands:\n");
 		printf("\t1\t - reads nand from ique to nand.bin/spare.bin\n");
+		printf("\t3 [file] - reads [file] from ique [to file]\n");
+#ifdef DEVELOPER_MODE
+		printf("\t4 [file] - write [file] to ique\n");
+#endif
+		printf("\t5 List all files on ique\n");
 		break;
 	case '1':
 		return CmdDumpNandRaw();
+		break;
+	case '3':
+		return CmdDumpFile(input + 1);
+		break;
+#ifdef DEVELOPER_MODE
+	case '4':
+		return CmdWriteFile(input + 1);
+		break;
+#endif
+	case '5':
+		return CmdGetDirListing();
 		break;
 
 	default:
